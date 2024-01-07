b12e0ebdee74f745a5aa77ef4cd6c80024eea6d3
Dr. Faisal Ayob <ahmadfaisal9@yahoo.com>
2024-01-07 08:10:59 +0800

    Update cursorMove.m

--- cursorMove.m
+++ cursorMove.m
@@ -1,3 +1,7 @@
+% Description: Moving Mouse to certain coordinate on the screen
+% Ahmad Faisal Mohamad Ayob, 2024
+% Source: https://www.mathworks.com/matlabcentral/answers/331746-how-to-get-the-real-time-position-of-mouse-outside-matlab#comment_439964
+
 import java.awt.Robot;
 import java.awt.event.*;
 mouse = Robot;
